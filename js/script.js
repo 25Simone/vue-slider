b5fa69b8aa63ebf4fb3f9570229fc150cc1e796a
25Simone <dantsimone111@gmail.com>
2022-01-14 16:38:36 +0100

    implementazione methods

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ new Vue({
             'https://unsplash.it/600/400?image=240',
             'https://unsplash.it/600/400?image=3',
             'https://unsplash.it/600/400?image=137',
-            'https://unsplash.it/600/400?image=345',
+            'https://unsplash.it/600/400?image=347',
             'https://unsplash.it/600/400?image=28',
         ],
         currentIndex: 0,
@@ -13,16 +13,10 @@ new Vue({
 
     methods: {
         nextImg: function() {
-            this.currentIndex++;
-            if(this.currentIndex > this.images.length - 1){
-                this.currentIndex = 0;
-            }
+            this.currentIndex = this.currentIndex >= this.images.length - 1 ? 0 : this.currentIndex + 1;
         },
         prevImg: function() {
-            this.currentIndex--;
-            if(this.currentIndex < 0){
-                this.currentIndex = this.images.length - 1;
-            }
+            this.currentIndex = this.currentIndex <= 0 ? this.images.length-1 : this.currentIndex - 1;
         },
     },
 })
