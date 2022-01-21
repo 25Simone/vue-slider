d63f2a31ec366939ac1d55c5006eea353a7d8a0a
25Simone <dantsimone111@gmail.com>
2022-01-21 17:22:03 +0100

    add click function on the circles slider

--- js/script.js
+++ js/script.js
@@ -11,8 +11,9 @@ new Vue({
             'https://unsplash.it/600/400?image=58',
         ],
         currentIndex: 0,
+        mosueOver: false,
+        timer: null,
     },
-
     methods: {
         nextImg: function() {
             this.currentIndex = this.currentIndex >= this.images.length - 1 ? 0 : this.currentIndex + 1;
@@ -21,12 +22,23 @@ new Vue({
             this.currentIndex = this.currentIndex <= 0 ? this.images.length-1 : this.currentIndex - 1;
         },
         automaticChange: function() {
-            const clock = setInterval( () => {
+            this.timer = setInterval( () => {
                 this.currentIndex = this.currentIndex >= this.images.length - 1 ? 0 : this.currentIndex + 1;
             },2000)
         },
+        mouseOverFunction: function(){
+            this.mosueOver = true;
+            clearInterval(this.timer);
+        },
+        mouseLeaveFunction: function(){
+            this.mouseOver = false;
+            this.automaticChange();
+        },
+        goToImage(index){
+            this.currentIndex = index;
+        }
     },
-    created() {
+    mounted() {
         this.automaticChange()
-    }
+    },
 })
